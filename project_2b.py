675909eaac8cf5f7014cf6eeff4f57e585ad7d24
Sebastian Holmin <sebastian.holmin@hotmail.com>
2020-12-02 10:01:17 +0100

    Progress on 2b

--- project_2b.py
+++ project_2b.py
@@ -6,8 +6,8 @@ import GPy
 from ase.constraints import FixAtoms, FixedLine
 from ase.optimize import BFGS
 from ase.visualize import view
-# from asap3 import EMT
-from ase.calculators.emt import EMT
+from asap3 import EMT
+# from ase.calculators.emt import EMT
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -65,10 +65,35 @@ def calculate_adatom_energy(surface, position):
     energy = surface_with_adatom.get_potential_energy()
     return energy
 
+def calc_surface_energy(surface):
+    # add adatom
+    surface_with_adatom = surface.copy()
+
+    # attach calculator
+    calc = EMT()
+    surface_with_adatom.set_calculator(calc)
+
+    # apply constraints
+    constraints = []
+    c = FixAtoms(indices=list(range(len(surface_with_adatom) - 1)))
+    constraints.append(c)
+    c = FixedLine(-1, [0, 0, 1])
+    constraints.append(c)
+
+    # relax configuration
+    surface_with_adatom.set_constraint(constraints)
+    dyn = BFGS(surface_with_adatom, logfile=None)
+    dyn.run(fmax=0.02, steps=200)
+
+    energy = surface_with_adatom.get_potential_energy()
+    return energy
+
+
 #%% Task 1: Analyzing the PES (1.5p)
 from ase.io import read
 surface = read('structures/surface_supercell.xyz')
-size = 50
+E_surface = calc_surface_energy(surface)
+size = 100
 xmax=16.65653
 ymax=2.884996
 E=np.empty((size,size))
@@ -76,6 +101,8 @@ E=np.empty((size,size))
 x = np.arange(xmax/(size+1),xmax,xmax/(size+1))
 y = np.arange(ymax/(size+1),ymax,ymax/(size+1))
 z = surface.get_positions()[:, 2].max() + 3
+
+#%%
 
 for xi in range(x.shape[0]):
     for yi in range(y.shape[0]):
@@ -90,29 +117,112 @@ for xi in range(x.shape[0]):
 # plt.show()
 plt.contourf(x,y,E, cmap='hot')
 plt.colorbar()
-plt.savefig()
-#%%
-k1 = GPy.kern.RBF(input_dim=2)
-k1['lengthscale'].constrain_bounded(0.1, 5)
-k2 = GPy.kern.Bias(input_dim=2)
-kernel = k1 + k2
-model = GPy.models.GPRegression(x_data, y_data, kernel)
-model.optimize()
-print(model)
-#model.optimize_restarts()
-#%%
-def min_fun(pos):
-    return calculate_adatom_energy(surface,(pos[0], pos[1], z))
+plt.savefig(fname='heatmap.pdf')
 
 #%%
-num_runs=3
+def min_fun(pos):
+    x=pos[0]%xmax
+    y=pos[1]%ymax
+    return calculate_adatom_energy(surface,(x, y, z))
+
+#%%
+num_runs=200
 start=np.random.rand(2,num_runs)
 startx=start[0]*xmax
 starty=start[1]*ymax
 E_local = np.empty(num_runs)
+x_local = np.empty((num_runs,2))
+result_local = []
 for i in range(num_runs):
     # minimize(min_fun, [startx[i], starty[i]])
-    minimize(lambda Pos: calculate_adatom_energy(surface, (Pos[0],Pos[1],z)), [startx[i], starty[i]])
+    result_local.append(minimize(lambda Pos: calculate_adatom_energy(surface, (Pos[0],Pos[1],z)), [startx[i], starty[i]]))
+    E_local[i]=result_local[i].fun
+    x_local[i]=result_local[i].x
+    print("i=",i)
+# %%
+plt.hist(E_local)
+plt.show()
+plt.scatter(x_local[:,0]%xmax,x_local[:,1]%ymax)
+plt.show()
+a,b,c,d=plt.hist2d(x_local[:,0]%xmax,x_local[:,1]%ymax,bins=50)
+print(a[a!=0])
+plt.colorbar()
+plt.show()
+# %%
+plt.contourf(x,y,E, cmap='hot')
+#plt.scatter((x_local[:,1]%ymax)*xmax/ymax,(x_local[:,0]%xmax)*ymax/xmax)
+plt.scatter(x_local[:,0],x_local[:,1])
+plt.xlim((0,xmax))
+plt.ylim((0,ymax))
+#%%
+plt.contourf(x,y,np.transpose(E)-E_surface, cmap='hot')
+plt.colorbar()
+plt.scatter(x_local[:,0]%xmax,x_local[:,1]%ymax)
 
 
+#%% Task 3, Start samples
+np.random.seed(123)
+num_runs=5
+start=np.random.rand(num_runs,2)
+startx=start[0]*xmax
+starty=start[1]*ymax
+E_data = np.empty((num_runs,1))
+x_data = start
+result_task3 = []
+for i in range(num_runs):
+    # minimize(min_fun, [startx[i], starty[i]])
+    #result_task3.append(minimize(lambda Pos: calculate_adatom_energy(surface, (Pos[0],Pos[1],z)), [startx[i], starty[i]]))
+    E_data[i]=calculate_adatom_energy(surface, (start[i,0],start[i,1],z))
+    #x_data[i,:]=start[i]
+
+
+# %%
+beta = 3
+def neg_A(x, model):
+    mu,sigma = model.predict(np.array(x,ndmin=2))
+    return (mu-beta*sigma)[0]
+
+def new_sample(model):
+    num_runs=20
+    start=np.random.rand(num_runs,2)*np.array([xmax,ymax])
+    #start=np.random.rand(2,num_runs)*np.array([xmax,ymax])
+    startx=start[0]*xmax
+    starty=start[1]*ymax
+    E_data = np.empty(num_runs)
+    x_data = np.empty((num_runs,2))
+    result_task3 = []
+    for i in range(num_runs):
+        result_task3.append(minimize(neg_A, start[i],args=model,bounds=[(0,xmax),(0,ymax)]))
+        E_data[i]=result_task3[i].fun
+        x_data[i]=result_task3[i].x
+    #model.optimize_restarts()
+
+    return start[np.argmin(E_data)], calculate_adatom_energy(surface, (start[np.argmin(E_data),0],start[np.argmin(E_data),1],z))
+# %%
+np.random.seed(2)
+k1 = GPy.kern.RBF(input_dim=2)
+k1['lengthscale'].constrain_bounded(0.1, 5)
+# k1['lengthscale'].set_prior(GPy.priors.Gamma(a=2, b=1))
+k2 = GPy.kern.Bias(input_dim=2)
+kernel = k1 + k2
+
+num_runs3=10
+
+for i in range(num_runs3):
+    model = GPy.models.GPRegression(x_data, E_data, kernel)
+    model.optimize()
+    
+    x_new, E_new=new_sample(model)
+    x_data=np.append(x_data,np.array(x_new,ndmin=2),axis=0)
+    E_data=np.append(E_data,np.array(E_new,ndmin=2),axis=0)
+
+
+#print(model)
+#model.
+#mu,sigma = model.predict(np.array((1,1),ndmin=2))
+
+plt.scatter(x_data[:,0],x_data[:,1])
+
+# %%
+1+1
 # %%
